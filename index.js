fa2de737b59186e98c656984521efa5e898f9203
anthony.culdora <a18.culdora@gmail.com>
2023-03-10 02:26:48 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,160 @@
+const linkPreviews = document.querySelectorAll('.link-preview1');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview1:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview1:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview2');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview2:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview2:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview3');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview3:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview3:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview4');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview4:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview4:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview5');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview5:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview5:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview6');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview6:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview6:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview7');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview7:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview7:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview8');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview8:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview8:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview9');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview9:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview9:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
+const linkPreviews = document.querySelectorAll('.link-preview10');
+
+linkPreviews.forEach(link => {
+  const image = link.dataset.image;
+  
+  link.addEventListener('mouseover', () => {
+    const preview = document.querySelector('.link-preview10:hover:after');
+    preview.style.backgroundImage = `url('${image}')`;
+  });
+  
+  link.addEventListener('mouseout', () => {
+    const preview = document.querySelector('.link-preview10:hover:after');
+    preview.style.backgroundImage = '';
+  });
+});
+// ------------------------------------------------------------------------
